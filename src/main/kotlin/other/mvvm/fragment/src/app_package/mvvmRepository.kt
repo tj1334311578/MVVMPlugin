1f33b8daec3798b076439dc2bb151452f3538275
tj <1334311578@qq.com>
2022-02-10 20:44:52 +0800

    修改api参数名

--- src/main/kotlin/other/mvvm/fragment/src/app_package/mvvmRepository.kt
+++ src/main/kotlin/other/mvvm/fragment/src/app_package/mvvmRepository.kt
@@ -10,7 +10,7 @@ fun mvvmRepository(
 package $packageName
 import javax.inject.Inject
 // 网络请求,数据缓存等,都在此类处理,通常为挂起函数
-class ${activityClass}Repository @Inject constructor(${if (apiClass.isNullOrBlank()) { "" }else{"private val repository:$apiClass"}}){
+class ${activityClass}Repository @Inject constructor(${if (apiClass.isNullOrBlank()) { "" }else{"private val api:$apiClass"}}){
 
 }
 """
